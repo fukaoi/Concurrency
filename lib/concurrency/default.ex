ead219edade1d8798b5dfbb72e88df3100e37d29
fukaoi <goodakira7@gmail.com>
2016-02-09 22:10:44 +0900

    modifed

--- lib/concurrency/default.ex
+++ lib/concurrency/default.ex
@@ -7,20 +7,22 @@ defmodule Concurrency.Default do
     @urls |> Enum.map(&HttpUtil.fetchHttpStatus(&1))
   end
 
+  def conCurrentHttpGet(pids, urls)
+
   def conCurrentHttpGet([], _) do end
 
   def conCurrentHttpGet([pid|other_pids], [url|other_url]) do
-    GenServer.cast(pid, {:http, url})
+    GenServer.cast(pid, {:http_fetch, url})
     conCurrentHttpGet(other_pids, other_url)
   end
 
-  def handle_cast({:http, url}, _) do
+  def handle_cast({:http_fetch, url}, _) do
     {:noreply, HttpUtil.fetchHttpStatus(url)}
   end
 
-  defp start_links(0, lists), do: lists
+  def start_links(0, lists), do: lists
 
-  defp start_links(count, lists \\ []) do
+  def start_links(count, lists \\ []) do
     {:ok, pid} = GenServer.start_link(__MODULE__, nil)
     start_links(count - 1, lists ++ [pid])
   end
