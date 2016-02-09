b5406e237b6c377d8511f29e56933aa54de7c0d2
fukaoi <goodakira7@gmail.com>
2016-02-10 00:09:58 +0900

    wip

--- lib/concurrency/default.ex
+++ lib/concurrency/default.ex
@@ -2,12 +2,14 @@ defmodule Concurrency.Default do
   use GenServer
 
   @urls HttpUtil.getUrlList
+  @call_or_call ""
 
   def single_start do
     @urls |> Enum.map(&HttpUtil.fetchHttpStatus(&1))
   end
 
-  def start do
+  def start(call_or_call \\ "call") do
+    @call_or_call = "call"
     Enum.count(@urls) |> start_links |> conCurrentHttpGet(@urls)
   end
 
@@ -16,11 +18,18 @@ defmodule Concurrency.Default do
   defp conCurrentHttpGet([], _) do end
 
   defp conCurrentHttpGet([pid|other_pids], [url|other_url]) do
-    GenServer.cast(pid, {:http_fetch, url})
+    case @call_or_call do
+      "call" -> GenServer.call(pid, {:fetch_sync, url})
+      "cast" -> GenServer.cast(pid, {:fetch_async, url})
+    end
     conCurrentHttpGet(other_pids, other_url)
   end
 
-  def handle_cast({:http_fetch, url}, _) do
+  def handle_call({:fetch_sync, url}, _, connection_dict) do
+    {:reply, HttpUtil.fetchHttpStatus(url), connection_dict}
+  end
+
+  def handle_cast({:fetch_async, url}, _) do
     {:noreply, HttpUtil.fetchHttpStatus(url)}
   end
 
